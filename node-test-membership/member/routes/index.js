a9dfe7d4716cd2eb38c110e55cfeb507f0da28e4
wilsonwww <honshengwen@gmail.com>
2020-11-25 16:57:39 +0800

    init member

---
+++ node-test-membership/member/routes/index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
+
+// var express = require("express");
+// var router = express.Router();
+
+// router.post("/", function (req, res, next) {
+//   console.log(req.body.test);
+// });
+
+// module.exports = router;
